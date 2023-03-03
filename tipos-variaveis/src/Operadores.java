04e0ffa4e443018431e248c1cfb551aad9c06c00
Moises Moraes <moisesmoraes7808@gmail.com>
2023-03-02 21:11:56 -0300

    tipos de variaveis

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,11 +1,11 @@
 public class Operadores {
 
-    public static void main(String[]) args) {
+    public static void main(String[] args) {
 
-         String nomeCompleto = "LINGUAGEM" + "JAVA"
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
 
-         System.out.println(nomeCompleto);
+        System.out.println(nomeCompleto);
 
     }
-    
+
 }
